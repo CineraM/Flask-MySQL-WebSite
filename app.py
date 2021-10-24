3ece630fda0bcbd3a9e16c445d7af8c7916fa872
CineraM <64340009+CineraM@users.noreply.github.com>
2021-10-24 18:10:31 -0400

    Code-Update

--- app.py
+++ app.py
@@ -1,18 +1,43 @@
-from flask import Flask, render_template
-import mysql.connector
-app = Flask(__name__)
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    passwd="admin",
-    database="test"
-)
+from flask import Flask, render_template, url_for, request, redirect
+import sql_commands
 
-cursor = mydb.cursor()
+app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    cursor.execute("SELECT * FROM dummy")
-    results = cursor.fetchall()
-    return render_template('home.html', results=results)
+@app.route('/home')  # home root
+def home_page():
+    return render_template('home.html')
+
+
+@app.route('/member')  # display all members root
+def members():
+    members = sql_commands.get_members()
+    return render_template('member.html', results=members)
+
+
+# member registration loop
+@app.route('/register_member', methods=['GET', 'POST'])
+def register_member():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        # date time function needs integers to work
+        year = int(request.form['year'])
+        month = int(request.form['month'])
+        day = int(request.form['day'])
+        # date time function needs integers to work
+        height = request.form['height']
+        weight = request.form['weight']
+
+        sql_commands.create_member(
+            username, password, firstname, lastname, year, month, day, height, weight)
+        return redirect(url_for('members'))  # references name of function ^
+
+    return render_template('register_member.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ sql_commands.py
@@ -0,0 +1,37 @@
+import mysql.connector
+import time
+from datetime import datetime
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="admin",
+    database="gym"
+)
+
+mycursor = mydb.cursor()
+
+
+def get_members():  # Returns a list, flask formats it into the html
+    mycursor.execute('SELECT * FROM member;')
+    data = mycursor.fetchall()
+    return data
+
+
+def create_member(username, password, firstname, lastname, year, month, day, height, weight):
+    date = datetime(year, month, day)
+    # date has to be passed in this format
+    formatted_date = date.strftime('%Y-%m-%d')
+    sql = """INSERT INTO member (username,
+        password,
+       firstname,
+       lastname,
+       dob,
+       height,
+       weight
+       ) VALUES(%s, %s, %s, %s, %s, %s, %s)"""
+    val = (username, password, firstname, lastname,
+           formatted_date, height, weight)
+    mycursor.execute(sql, val)
+    mydb.commit()  # execute and commit to the database
+# we will probably need to add a try and catch for input validation
